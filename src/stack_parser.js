d19d86543043bbd1016c4efdb36a22d937ef5210
SovietBear <trekkersforever@gmail.com>
2017-02-23 15:24:28 -0700

    add stack_parser.remove_first_statement

--- src/stack_parser.js
+++ src/stack_parser.js
@@ -1,5 +1,6 @@
 module.exports = {
-    extract_first_statement: extract_first_statement
+    extract_first_statement: extract_first_statement,
+    remove_first_statement: remove_first_statement
 }
 
 function extract_first_statement(stack, separator) {
@@ -8,4 +9,21 @@ function extract_first_statement(stack, separator) {
     }
 
     return stack.substring(0, stack.indexOf(separator, 0))
+}
+
+function remove_first_statement(stack, separator) {
+    if (stack === undefined || stack === '' || !stack.includes(separator)) {
+        return ''
+    }
+
+    if (stack.includes(separator + separator)) {
+        stack = stack.replace(separator + separator, separator)
+    }
+
+    stack = stack.replace(extract_first_statement(stack, separator), '')
+    if (stack.startsWith(separator)) {
+        stack = stack.substring(1)
+    }
+
+    return stack.trim()
 }
--- tests/stack_parser.test.js
+++ tests/stack_parser.test.js
@@ -29,4 +29,30 @@ describe('stack_parser', function() {
             test_extract('Uncaught Exception: patient zero has started to spread!', ':', 'Uncaught Exception')
         })
     })
+
+    describe('remove_first_statement', function() {
+        let test_remove = (stack, separator, expected) => {
+            it('should return ' + expected + ' when given ' + stack + ' with separator ' + separator, () => {
+                let result = stack_parser.remove_first_statement(stack, separator)
+                assert.equal(result, expected)
+            })
+        }
+
+        describe('test undefined/empty', () => {
+            test_remove(undefined, ':', '')
+            test_remove('', ':', '')
+        })
+
+        describe('test separator not present', () => [
+            test_remove('asdf zxcv', ':', '')
+        ])
+
+        describe('test stack with multiple separators', () => {
+            test_remove('Uncaught Exception:: what was that whole one in a million talk?', ':', 'what was that whole one in a million talk?')
+        })
+
+        describe('test valid stack', () => {
+            test_remove('Uncaught Exception: the sun just exploded and we have 8 minutes to live!', ':', 'the sun just exploded and we have 8 minutes to live!')
+        })
+    })
 })
