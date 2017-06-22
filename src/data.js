f32440c3c0888143286348747fe3346c8b9e497d
SovietBear <trekkersforever@gmail.com>
2017-06-22 09:30:46 -0600

    move saved data load to data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,8 @@ var consts = require('./consts')
 
 module.exports = {
     check_valid_number: check_valid_number,
-    get_data_to_save: get_data_to_save
+    get_data_to_save: get_data_to_save,
+    load: load
 }
 
 function check_valid_number(bot, message, value) {
@@ -19,4 +20,15 @@ function get_data_to_save(json, new_days, new_hour, new_minute) {
     saved_data.hour = (new_hour !== undefined && new_hour !== '') ? new_hour : consts.get_hour()
     saved_data.minute = (new_minute !== undefined && new_minute !== '') ? new_minute : consts.get_minute()
     return saved_data
+}
+
+function load(channels) {
+    //TODO: This appears to have broken some time ago, figure out and fix
+    channels.get(consts.nanAlertChannelId, function(err, json) { 
+        if (json !== undefined && json.data.interval !== undefined) {
+            consts.set_days(json.data.interval)
+            consts.set_hour(json.data.hour)
+            consts.set_minute(json.data.minute)
+        }
+    })
 }
--- src/morbo.js
+++ src/morbo.js
@@ -7,6 +7,7 @@ var os = require('os')
 var channel_history_parser = require('./channel_history_parser.js')
 var consts = require('./consts.js')
 var commands = require('./commands.js')
+var data = require('./data.js')
 
 var controller = Botkit.slackbot({
     stats_output: true,
@@ -17,15 +18,7 @@ var bot = controller.spawn({
     token: 'xoxb-142124192757-vCae3E93BkPHLojEYEQzjU5U'
 }).startRTM()
 
-//TODO: This appears to have broken some time ago, figure out and fix
-controller.storage.channels.get(consts.nanAlertChannelId, function(err, json) { 
-    if (json !== undefined && json.data.interval !== undefined) {
-        consts.set_days(json.data.interval)
-        consts.set_hour(json.data.hour)
-        consts.set_minute(json.data.minute)
-    }
-})
-
+data.load(controller.storage.channels)
 commands.initialize(controller, bot)
 
 function post_message(message) {
