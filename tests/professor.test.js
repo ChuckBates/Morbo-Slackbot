778db85c377776781aac0cf47520935f7e353257
SovietBear <trekkersforever@gmail.com>
2017-07-03 23:58:53 -0600

    Implement handleOctopusMessage

--- tests/professor.test.js
+++ tests/professor.test.js
@@ -71,6 +71,73 @@ describe('professor', function() {
             testHandle(message, expected)
         })
     })
+
+    describe('handleOctopusMessage', function() {
+        let testHandle = (message, expected) => {
+            it('should return ' + JSON.stringify(expected) + ' when given message: ' + JSON.stringify(message), () => {
+                let result = professor.handleOctopusMessage(message)
+                assert.deepEqual(result, expected)
+            })
+        }
+
+        describe('when handling an octopus message that is empty', () => {
+            let message = {}
+            let expected = {
+                slackMessage: {},
+                code: 200,
+                message: 'okay'
+            }
+            testHandle(message, expected)
+        })
+
+        describe('when handling a failed/success octopus message', () => {
+            let message = messageMother()
+            let expected = {
+                slackMessage: { 
+                    channel: consts.botTestingChannelId, 
+                    username: 'Professor',
+                    as_user: false,
+                    icon_emoji: ':good-news:',
+                    text: 'Good news everyone!',
+                    attachments: [
+                        {
+                            fallback: message.attachments[0].fallback,
+                            title: message.attachments[0].title,
+                            title_link: message.attachments[0].title_link,
+                            text: message.attachments[0].text,
+                            color: message.attachments[0].color,
+                            fields: [
+                                {
+                                    title: message.attachments[0].fields[0].title,
+                                    value: message.attachments[0].fields[0].value,
+                                    short: message.attachments[0].fields[0].short
+                                },
+                                {
+                                    title: message.attachments[0].fields[1].title,
+                                    value: message.attachments[0].fields[1].value,
+                                    short: message.attachments[0].fields[1].short
+                                }
+                            ]
+                        }
+                    ]
+                },
+                code: 200,
+                message: 'success'
+            }
+            testHandle(message, expected)
+        })
+
+        describe('when handling a octopus message and there are no attachments', () => {
+            let message = messageMother()
+            message.attachments = []
+            let expected = {
+                slackMessage: [],
+                code: 200, 
+                message: 'okay'
+                }
+            testHandle(message, expected)
+        })
+    })
 })
 
 function messageMother() {
